1bab47715d975cf085b1b6244a0ba16e60f5ea2a
Talut Salako <tsalako491@stu.ui.edu.ng@users.noreply.github.com>
2023-07-08 13:51:30 +0100

    added function to generate node data

--- output.py
+++ output.py
@@ -2,12 +2,13 @@ import pandas as pd
 import calculation as calc
 
 count = 1
-index = range(1, len(calc.all_pressure) + 1)
+index = range(1, len(calc.all_pressure) - 1)
 heading = [
     "start node",
     "end node",
     "start node pressure",
-    "end node pressure" "flow",
+    "end node pressure",
+    "flow",
     "pipe diameter",
     "pipe length",
     "head loss",
@@ -42,14 +43,13 @@ for n in index:
             headings_data.append(entry)
 
         elif n == calc.no_of_sprinkler_on_branch_lines - 1:
-            length = (
-                calc.equiv_length_t(
-                    l=calc.DIST_OF_SPRINKLER_ON_BRANCH_LINES / 2,
-                    d_1=calc.br2_line_int_dia,
-                    d_2=calc.int_dia_sch40_2inch,
-                    fitt_equiv=calc.t_fitt_sch40_2inch,
-                ),
+            length = calc.equiv_length_t(
+                l=calc.DIST_OF_SPRINKLER_ON_BRANCH_LINES / 2,
+                d_1=calc.br2_line_int_dia,
+                d_2=calc.int_dia_sch40_2inch,
+                fitt_equiv=calc.t_fitt_sch40_2inch,
             )
+
             entry = [
                 n,
                 n + 1,
@@ -63,13 +63,11 @@ for n in index:
             headings_data.append(entry)
 
         elif n == calc.no_of_sprinkler_on_branch_lines:
-            length = (
-                calc.equiv_length_t(
-                    l=calc.dist_between_branch_lines,
-                    d_1=calc.cr_line_int_dia,
-                    d_2=calc.int_dia_sch40_3inch,
-                    fitt_equiv=calc.t_fitt_sch40_3inch,
-                ),
+            length = calc.equiv_length_t(
+                l=calc.dist_between_branch_lines,
+                d_1=calc.cr_line_int_dia,
+                d_2=calc.int_dia_sch40_3inch,
+                fitt_equiv=calc.t_fitt_sch40_3inch,
             )
             entry = [
                 n,
@@ -83,14 +81,13 @@ for n in index:
             entry.append(entry[3] - entry[2])
             headings_data.append(entry)
         elif n == calc.no_of_sprinkler_on_branch_lines + 1:
-            length = (
-                calc.equiv_length_t(
-                    l=calc.DIST_OF_SPRINKLER_ON_BRANCH_LINES / 2,
-                    d_1=calc.br_line_int_dia,
-                    d_2=calc.int_dia_sch40_1_5inch,
-                    fitt_equiv=calc.t_fitt_sch40_1_5inch,
-                ),
+            length = calc.equiv_length_t(
+                l=calc.DIST_OF_SPRINKLER_ON_BRANCH_LINES / 2,
+                d_1=calc.br_line_int_dia,
+                d_2=calc.int_dia_sch40_1_5inch,
+                fitt_equiv=calc.t_fitt_sch40_1_5inch,
             )
+
             entry = [
                 n,
                 n - 1,
@@ -105,7 +102,7 @@ for n in index:
             count += 1
 
     elif n > calc.no_of_sprinkler_on_branch_lines + 1:
-        p = (n % (calc.no_of_sprinkler_on_branch_lines + 1))
+        p = n % (calc.no_of_sprinkler_on_branch_lines + 1)
         if p == 0:
             p = calc.no_of_sprinkler_on_branch_lines + 1
         if p <= calc.no_of_sprinkler_on_branch_lines + 1:
@@ -137,14 +134,13 @@ for n in index:
                 headings_data.append(entry)
 
             elif p == calc.no_of_sprinkler_on_branch_lines - 1:
-                length = (
-                    calc.equiv_length_t(
-                        l=calc.DIST_OF_SPRINKLER_ON_BRANCH_LINES / 2,
-                        d_1=calc.br2_line_int_dia,
-                        d_2=calc.int_dia_sch40_2inch,
-                        fitt_equiv=calc.t_fitt_sch40_2inch,
-                    ),
+                length = calc.equiv_length_t(
+                    l=calc.DIST_OF_SPRINKLER_ON_BRANCH_LINES / 2,
+                    d_1=calc.br2_line_int_dia,
+                    d_2=calc.int_dia_sch40_2inch,
+                    fitt_equiv=calc.t_fitt_sch40_2inch,
                 )
+
                 entry = [
                     n,
                     n + 1,
@@ -159,14 +155,13 @@ for n in index:
 
             elif p == calc.no_of_sprinkler_on_branch_lines:
                 if count < calc.no_of_branch_lines:
-                    length = (
-                        calc.equiv_length_t(
-                            l=calc.dist_between_branch_lines,
-                            d_1=calc.cr_line_int_dia,
-                            d_2=calc.int_dia_sch40_3inch,
-                            fitt_equiv=calc.t_fitt_sch40_3inch,
-                        ),
+                    length = calc.equiv_length_t(
+                        l=calc.dist_between_branch_lines,
+                        d_1=calc.cr_line_int_dia,
+                        d_2=calc.int_dia_sch40_3inch,
+                        fitt_equiv=calc.t_fitt_sch40_3inch,
                     )
+
                     print(n)
                     print(count)
                     entry = [
@@ -181,14 +176,13 @@ for n in index:
                     entry.append(entry[3] - entry[2])
                     headings_data.append(entry)
             elif p == calc.no_of_sprinkler_on_branch_lines + 1:
-                length = (
-                    calc.equiv_length_t(
-                        l=calc.DIST_OF_SPRINKLER_ON_BRANCH_LINES / 2,
-                        d_1=calc.br_line_int_dia,
-                        d_2=calc.int_dia_sch40_1_5inch,
-                        fitt_equiv=calc.t_fitt_sch40_1_5inch,
-                    ),
+                length = calc.equiv_length_t(
+                    l=calc.DIST_OF_SPRINKLER_ON_BRANCH_LINES / 2,
+                    d_1=calc.br_line_int_dia,
+                    d_2=calc.int_dia_sch40_1_5inch,
+                    fitt_equiv=calc.t_fitt_sch40_1_5inch,
                 )
+
                 entry = [
                     n,
                     n - 1,
@@ -201,17 +195,37 @@ for n in index:
                 entry.append(entry[3] - entry[2])
                 headings_data.append(entry)
                 count += 1
-            
+
 
 print(headings_data)
 
 
-
 df = pd.DataFrame(headings_data, columns=heading, index=index)
 
-
+df.to_excel("pipe_data.xlsx", sheet_name="pipe_data")
 # index = np.arange(1, len(all_pressure)+1)
 # output_dict =
 # for n in range(1, len(all_pressure)+1):
 #     output_dict = {"start node": {n:}}
 #     df.to_csv('output/pre
+
+
+headings_data2 = []
+index2 = range(1, len(calc.all_pressure) + 1)
+print(len(calc.all_pressure))
+print(len(calc.all_discharge_rate))
+heading2 = [
+    "node point",
+    "pressure",
+    "discharge rate",
+]
+for n in index:
+    entry = [n, calc.all_pressure[n - 1], calc.all_discharge_rate[n - 1]]
+    
+    headings_data2.append(entry)
+
+print(len(headings_data2))
+print(headings_data2)
+# df2 = pd.DataFrame(headings_data2, columns=heading2, index=index2)
+
+append_df.to_excel("pipe_data.xlsx", df2)
